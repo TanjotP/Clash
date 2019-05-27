9dc5fd4c6c683ec7797e623ece58f5e1db63fad4
TanjotP <tanjotspanesar@gmail.com>
2019-05-26 22:49:09 -0400

    initial commit

---
+++ app/src/main/java/com/danceapp/clash/SelectAccountPage.kt
@@ -0,0 +1,12 @@
+package com.danceapp.clash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectAccountPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.select_account_page)
+    }
+}
